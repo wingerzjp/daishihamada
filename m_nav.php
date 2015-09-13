cfd24437c915a31e98e0cfecd5f02f867afa1528
wingerzjp <wingerzjp1979@gmail.com>
2015-09-13 18:36:38 +0900

    スマホ対応。navphp追加など

---
+++ m_nav.php
@@ -0,0 +1,43 @@
+<!-- Navbar -->
+<nav id="m_nav">
+	<div class="nav_container">
+	<!-- Right Control -->
+		<div class="nav_line">
+			<a href="#my-menu"><img src="menulogo.png" width="25" height="25"></a>
+		</div><!-- nav_line -->
+				
+		<!-- Logo -->
+		<div class="nav_logo">
+			<a href="./index.html"><img src="./images/newlogo.png" width="200" height="91"></a>
+		</div><!-- nav_logo -->
+	</div><!-- nav_container -->
+</nav>
+
+<!-- ▼右側から表示されるメニューまたはコンテンツ▼ -->
+<div class="sb-slidebar sb-right">
+  <nav id="my-menu">
+	  	<ul class="sb-menu">
+	   		<li>info</li>
+	        <li>biography</li>
+	        <li>photo</li>
+	        <li>works</li>
+	        <li>movie</li>
+	    	<li>contact</li>
+	        <div id="nav_icon">
+	        	<a href="http://instagram.com/daishihamada/" target="_blank">
+				<img src="./icon/instagram500 copy.png"></a>
+				<a href="https://www.facebook.com/daishi.hamada.fan" target="_blank">
+				<img src="./icon/facebook500 copy.png"></a>
+				<a href="https://twitter.com/DaishiHamada" target="_blank">
+				<img src="./icon/twitter copy.png"></a>
+				<a href="http://daishi-hamada.tumblr.com" target="_blank">
+				<img src="./icon/tumblr copy.png"></a>
+				<a href="https://www.flickr.com/photos/79730483@N02/" target="_blank">
+				<img src="./icon/flickr copy.png"></a>
+				<a href="http://ameblo.jp/dee-keep-smiling/" target="_blank">
+				<img src="./icon/blogger copy.png"></a>
+			</div><!-- nav_icon -->
+	     </ul>
+    </nav>
+</div><!-- sb-slidebar sb-right -->
+<!-- ▲右側から表示されるメニューまたはコンテンツ▲ -->
