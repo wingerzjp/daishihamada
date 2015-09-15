a8f0b7045bc2bc68a32983abcdd88e25c14b3494
wingerzjp <wingerzjp1979@gmail.com>
2015-09-16 00:56:35 +0900

    index/info/movieにsliders適用

--- sp_nav.php
+++ sp_nav.php
@@ -1,30 +1,6 @@
-<div data-role="panel" id="menu-right" data-position="right"  data-display="push">
-		<ul data-role="listview">
-			<li>info</li>
-	        <li>biography</li>
-	        <li>photo</li>
-	        <li>works</li>
-	        <li>movie</li>
-	    	<li>contact</li>
-	        <div id="nav_icon">
-	        	<a href="http://instagram.com/daishihamada/" target="_blank">
-				<img src="./icon/instagram500 copy.png"></a>
-				<a href="https://www.facebook.com/daishi.hamada.fan" target="_blank">
-				<img src="./icon/facebook500 copy.png"></a>
-				<a href="https://twitter.com/DaishiHamada" target="_blank">
-				<img src="./icon/twitter copy.png"></a>
-				<a href="http://daishi-hamada.tumblr.com" target="_blank">
-				<img src="./icon/tumblr copy.png"></a>
-				<a href="https://www.flickr.com/photos/79730483@N02/" target="_blank">
-				<img src="./icon/flickr copy.png"></a>
-				<a href="http://ameblo.jp/dee-keep-smiling/" target="_blank">
-				<img src="./icon/blogger copy.png"></a>
-			</div><!-- nav_icon -->
-		</ul>
-</div>
 <div id="m_nav">
-	<div class="nav_line">
-		<a href="#menu-right"><img src="menulogo.png"></a>
+	<div class="sb-toggle-left">
+		<img src="menulogo.png">
 	</div>
 	<div class="nav_logo">
 		<img src="./images/newlogo.png">
